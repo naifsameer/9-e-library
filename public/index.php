fc391fe0b72b39a12446e689b27feabdba55fbd6
Naif <nayef7138@gmail.com>
2022-03-24 00:05:42 +0300

    edit: router paths

--- public/index.php
+++ public/index.php
@@ -1,13 +1,20 @@
 <?php
 
-use app\controllers\PostController;
+use app\controllers\AuthController;
+use app\controllers\DashboardController;
+use app\controllers\HomeController;
 use app\Router;
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$router = new Router();
+Router::get('/', [HomeController::class, 'index']);
 
-$router->get('/', [PostController::class, 'index']);
+Router::get('/login', [AuthController::class, 'login']);
+Router::post('/login', [AuthController::class, 'store']);
+Router::get('/logout', [AuthController::class, 'logout']);
 
 
-$router->resolve();
+Router::get('/dashboard', [DashboardController::class, 'index']);
+
+Router::resolve();
