9267a3ed62e8c137aa4cb92273d6d737adc40aea
Naif <nayef7138@gmail.com>
2022-03-24 00:03:10 +0300

    add: Model and Auth

---
+++ models/Auth.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+use app\helpers\SessionHelper;
+
+SessionHelper::run_session();
+
+use app\Database;
+use app\helpers\UtilHelper;
+
+/**
+ * Auth
+ * @package app\models
+ */
+class Auth extends Model
+{
+  public static function login($email, $password)
+  {
+    $user_data  = self::table('users')
+      ->select("email", "usersID", "password")
+      ->where('email', $email)
+      ->and('password', $password)
+      ->get();
+
+    if ($user_data) {
+      SessionHelper::setSession(SessionHelper::$AUTH, true);
+      SessionHelper::setSession(SessionHelper::$USER_ID, $user_data['usersID']);
+      return true;
+    }
+
+    return false;
+  }
+}
---
+++ models/Model.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models;
+
+use app\Database;
+
+class Model extends Database
+{
+  private function __construct()
+  {
+  }
+
+  public static function table(string $table_name)
+  {
+    return Database::getInstance()->setTable($table_name);
+  }
+}
