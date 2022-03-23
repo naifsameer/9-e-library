592d5ef5b859567053c7f75769cbeab9e8a5be66
Naif <nayef7138@gmail.com>
2022-03-24 00:05:22 +0300

    add autoload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '80d8badf035f33e12e33b9522011eca266ed290b',
         'name' => 'app/blog',
         'dev' => true,
     ),
     'versions' => array(
         'app/blog' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '80d8badf035f33e12e33b9522011eca266ed290b',
             'dev_requirement' => false,
         ),
     ),
