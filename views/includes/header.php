1cfeeae080065a401b676ebf072e967fab47ff84
Naif <nayef7138@gmail.com>
2022-03-26 22:45:12 +0300

    update: move each component to its file

---
+++ views/includes/header.php
@@ -0,0 +1,88 @@
+<!-- header section -->
+<header class="c-header">
+  <!-- header menu section -->
+  <div class="c-header-menu">
+    <div class="container">
+      <ul class="list">
+        <li class="item">
+          <button class="js-show-signup-modal">
+            <i class="fa fa-user"></i>
+            ادخل لحسابك أو سجل الآن
+          </button>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-star"></i>
+            المفضلة
+          </a>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-bus"></i>
+
+            تتبع الشحنة
+          </a>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-save"></i>
+
+            طلبات الصيانة
+          </a>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-info-circle"></i>
+
+            المساعدة
+          </a>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-podcast"></i>
+
+            فروعنا
+          </a>
+        </li>
+        <li class="item">
+          <a href="">
+            <i class="fa fa-cloud-moon"></i>
+            المملكة العربية السعودية | ر.س
+          </a>
+        </li>
+        <li class="item">
+          <button data-lang="ar" class="lang-btn">
+            <i class="fa fa-language"></i>
+            English
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- nav -->
+  <div class="container">
+    <nav class="c-nav">
+      <!-- logo -->
+      <a href="/" class="logo">
+        <img src="images/icons/logo.svg" alt="" />
+      </a>
+
+      <form autocomplete="off" action="" class="form autocomplete">
+        <input id="myInput" type="text" name="search-input" placeholder="search...." />
+
+        <button>
+          <i class="fa fa-search"></i>
+        </button>
+      </form>
+
+      <a href="/checkout">
+        <button class="shopping-btn">
+          <i class="fa fa-shopping-cart"></i>
+
+          <span class="shopping-item-number"></span>
+        </button>
+      </a>
+    </nav>
+  </div>
+</header>
